f5182b450b60c21c208f8a340b3dccedec2adc99
dev-luis-la <92961052+dev-luis-la@users.noreply.github.com>
2022-03-17 14:41:02 -0400

    creating firebase

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,34 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBtNS3VdU6ZDe2iHmfD59UXi-qI4a6doR8",
+    authDomain: "plant-db-66a26.firebaseapp.com",
+    projectId: "plant-db-66a26",
+    storageBucket: "plant-db-66a26.appspot.com",
+    messagingSenderId: "546986862204",
+    appId: "1:546986862204:web:aa7e2f67666af7b4a94bf9",
+    measurementId: "G-WKG3ZBQLFD"
+  };
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  console.log(userAuth);
+};
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
