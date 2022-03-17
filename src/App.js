34fc8c8395bf0c93cfea47bcb526f4bad422ed0f
dev-luis-la <92961052+dev-luis-la@users.noreply.github.com>
2022-03-17 12:26:48 -0400

    creating directory components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
-import './categories.styles.scss';
+import Directory from "./components/directory/directory.component";
+
 
 const App = () => {
 
   const categories = [
     {
-      id: '1',
-      title: 'Monstera',
+      "id": 1,
+      "title": "Monsdteras",
+      "imageUrl": "https://images.unsplash.com/photo-1545165375-1b744b9ed444?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
     },
     {
-      id: '2',
-      title: 'Monstera',
+      "id": 2,
+      "title": "Succulents",
+      "imageUrl": "https://images.unsplash.com/photo-1551893665-f843f600794e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
     },
     {
-      id: '3',
-      title: 'Monstera',
+      "id": 3,
+      "title": "Indooor Plants",
+      "imageUrl": "https://images.unsplash.com/photo-1567225557594-88d73e55f2cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
     },
     {
-      id: '4',
-      title: 'Monstera',
+      "id": 4,
+      "title": "Outdoor Plants",
+      "imageUrl": "https://images.unsplash.com/photo-1581773340073-0d6c32f500a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
     },
+    {
+      "id": 5,
+      "title": "Pots",
+      "imageUrl": "https://m.media-amazon.com/images/S/aplus-media/sc/5e4b87aa-d59e-4643-ba23-2b0f9bbe2637.__CR0,0,970,600_PT0_SX970_V1___.jpg"
+    }
   ]
+  
 
   return (
-    <div className="categories-container">
-    {categories.map(({title}) =>(
-      <div className="category-container">
-      <div className= "background-image" />
-      <div className="category-body-container">
-        <h2>{title}</h2>
-        <p>Shop now</p>
-      </div>
-    </div>
-    ))}
-   </div>
+      <Directory  categories={categories}/>
   );
 }
 
