3c1c9f62e12d9c839a7413faeed5e3fd13057d05
dev-luis-la <92961052+dev-luis-la@users.noreply.github.com>
2022-03-19 02:23:33 -0400

    creating firestore email&password auth

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,8 +4,13 @@ import {
   signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
+  createUserWithEmailAndPassword,
 } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import { 
+  getFirestore,
+   doc,
+   getDoc,
+    setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyBtNS3VdU6ZDe2iHmfD59UXi-qI4a6doR8",
@@ -19,18 +24,21 @@ const firebaseConfig = {
 
   const firebaseApp = initializeApp(firebaseConfig);
 
-  const provider = new GoogleAuthProvider();
+  const googleProvider = new GoogleAuthProvider();
   
-  provider.setCustomParameters({
+  googleProvider.setCustomParameters({
     prompt: 'select_account',
   });
   
   export const auth = getAuth();
-  export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+  export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+  export const signInWithGoogleRedirect = () => signInWithRedirect(auth,googleProvider);
   
   export const db = getFirestore();
   
   export const createUserDocumentFromAuth = async (userAuth) => {
+    if(!userAuth) return;
+    
     const userDocRef = doc(db, 'users', userAuth.uid);
   
     const userSnapshot = await getDoc(userDocRef);
@@ -52,3 +60,11 @@ const firebaseConfig = {
   
     return userDocRef;
   };
+
+
+  export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return;
+
+     createUserWithEmailAndPassword(auth, email, password);
+
+  }
